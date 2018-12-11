23d28870c2b0e613f52d59cd05ea65992ecee773
RoundBearChoi <roundbeargames@gmail.com>
2018-12-11 18:53:50 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Characters/PathFinding/PathFinder.cs
+++ Assets/Roundbeargames/RB_Characters/PathFinding/PathFinder.cs
@@ -3,34 +3,26 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
-namespace roundbeargames
-{
-    public class PathFinder : MonoBehaviour
-    {
+namespace roundbeargames {
+    public class PathFinder : MonoBehaviour {
         public GameObject WayPointPrefab;
+        public bool ShowDebugRaycast;
+        public Vector3 GreenLineOffset;
 
-        [Button(ButtonSizes.Medium)]
-        private void ToggleRender()
-        {
-            MeshRenderer[] renderers = this.gameObject.GetComponentsInChildren<MeshRenderer>();
+        [Button (ButtonSizes.Medium)]
+        private void ToggleRender () {
+            MeshRenderer[] renderers = this.gameObject.GetComponentsInChildren<MeshRenderer> ();
 
-            if (RenderersOn)
-            {
+            if (RenderersOn) {
                 RenderersOn = false;
-            }
-            else
-            {
+            } else {
                 RenderersOn = true;
             }
 
-            foreach (MeshRenderer r in renderers)
-            {
-                if (RenderersOn)
-                {
+            foreach (MeshRenderer r in renderers) {
+                if (RenderersOn) {
                     r.enabled = false;
-                }
-                else
-                {
+                } else {
                     r.enabled = true;
                 }
             }
@@ -39,84 +31,69 @@ namespace roundbeargames
         public List<WayPoint> Unvisited;
         public List<WayPoint> ResultPath;
 
-        [HorizontalGroup("Split", 0.5f)]
-        [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
-        private void CreateWayPointFarLeft()
-        {
-            GetLatestWayPoint().CreateWayPointLeft();
+        [HorizontalGroup ("Split", 0.5f)]
+        [Button (ButtonSizes.Large), GUIColor (0.4f, 0.8f, 1)]
+        private void CreateWayPointFarLeft () {
+            GetLatestWayPoint ().CreateWayPointLeft ();
         }
 
-        [VerticalGroup("Split/right")]
-        [Button(ButtonSizes.Large), GUIColor(0, 1, 0)]
-        private void CreateWayPointFarRight()
-        {
-            GetLatestWayPoint().CreateWayPointRight();
+        [VerticalGroup ("Split/right")]
+        [Button (ButtonSizes.Large), GUIColor (0, 1, 0)]
+        private void CreateWayPointFarRight () {
+            GetLatestWayPoint ().CreateWayPointRight ();
         }
 
-        public WayPoint GetLatestWayPoint()
-        {
-            WayPoint[] wArray = this.gameObject.GetComponentsInChildren<WayPoint>();
+        public WayPoint GetLatestWayPoint () {
+            WayPoint[] wArray = this.gameObject.GetComponentsInChildren<WayPoint> ();
             return wArray[wArray.Length - 1];
         }
 
-        private void ResetUnvisited()
-        {
-            Unvisited.Clear();
-            WayPoint[] all = this.gameObject.GetComponentsInChildren<WayPoint>();
-            foreach (WayPoint w in all)
-            {
+        private void ResetUnvisited () {
+            Unvisited.Clear ();
+            WayPoint[] all = this.gameObject.GetComponentsInChildren<WayPoint> ();
+            foreach (WayPoint w in all) {
                 w.PreviousPoint = null;
                 w.KnownDistance = 1000;
 
-                Unvisited.Add(w);
+                Unvisited.Add (w);
 
-                if (w.pathFinder == null)
-                {
+                if (w.pathFinder == null) {
                     w.pathFinder = this;
                 }
             }
         }
 
-        public List<WayPoint> FindPath(WayPoint start, WayPoint end)
-        {
-            ResetUnvisited();
-            ResultPath.Clear();
+        public List<WayPoint> FindPath (WayPoint start, WayPoint end) {
+            ResetUnvisited ();
+            ResultPath.Clear ();
 
             start.KnownDistance = 0;
-            CalcDistance(start);
+            CalcDistance (start);
 
-            AddResult(start, end);
+            AddResult (start, end);
 
             return ResultPath;
         }
 
-        private void AddResult(WayPoint start, WayPoint end)
-        {
-            ResultPath.Add(end);
-            if (start != end)
-            {
-                try
-                {
-                    AddResult(start, end.PreviousPoint);
-                }
-                catch (System.Exception e)
-                {
-                    Debug.LogError(e);
+        private void AddResult (WayPoint start, WayPoint end) {
+            ResultPath.Add (end);
+            if (start != end) {
+                try {
+                    AddResult (start, end.PreviousPoint);
+                } catch (System.Exception e) {
+                    Debug.LogError (e);
                 }
             }
         }
 
-        private void CalcDistance(WayPoint point)
-        {
+        private void CalcDistance (WayPoint point) {
             //Debug.Log ("--- checking " + point.gameObject.name + " ---");
-            point.CalcNeighborDistance();
-            Unvisited.Remove(point);
+            point.CalcNeighborDistance ();
+            Unvisited.Remove (point);
 
-            foreach (WayPoint n in point.Neighbors)
-            {
-                if (Unvisited.Contains(n))
-                {
-                    CalcDistance(n);
+            foreach (WayPoint n in point.Neighbors) {
+                if (Unvisited.Contains (n)) {
+                    CalcDistance (n);
                 }
             }
         }
--- Assets/Roundbeargames/RB_Characters/PathFinding/WayPoint.cs
+++ Assets/Roundbeargames/RB_Characters/PathFinding/WayPoint.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine;
 
-namespace roundbeargames
-{
-    public class WayPoint : MonoBehaviour
-    {
+namespace roundbeargames {
+    public class WayPoint : MonoBehaviour {
         public PathFindMethod pathFindMethod;
         public float RequiredJumpForce;
         public float AirWalkSpeedMultiplier;
@@ -20,62 +18,72 @@ namespace roundbeargames
         public Color DefaultColor;
         private Renderer renderer;
 
-        void Start()
-        {
-            renderer = GetComponent<Renderer>();
-            TurnOnDefaultColor();
+        [HorizontalGroup ("Split", 0.5f)]
+        [Button (ButtonSizes.Large), GUIColor (0.4f, 0.8f, 1)]
+        public void CreateWayPointLeft () {
+            CreateWayPointPrefab (new Vector3 (-0.6f, 0f, 0f));
         }
 
-        [HorizontalGroup("Split", 0.5f)]
-        [Button(ButtonSizes.Large), GUIColor(0.4f, 0.8f, 1)]
-        public void CreateWayPointLeft()
-        {
-            CreateWayPointPrefab(new Vector3(-0.6f, 0f, 0f));
+        [VerticalGroup ("Split/right")]
+        [Button (ButtonSizes.Large), GUIColor (0, 1, 0)]
+        public void CreateWayPointRight () {
+            CreateWayPointPrefab (new Vector3 (0.6f, 0f, 0f));
         }
 
-        [VerticalGroup("Split/right")]
-        [Button(ButtonSizes.Large), GUIColor(0, 1, 0)]
-        public void CreateWayPointRight()
-        {
-            CreateWayPointPrefab(new Vector3(0.6f, 0f, 0f));
+        void Start () {
+            renderer = GetComponent<Renderer> ();
+            TurnOnDefaultColor ();
         }
 
-        void CreateWayPointPrefab(Vector3 offset)
-        {
-            if (pathFinder == null)
-            {
-                pathFinder = this.gameObject.GetComponentInParent<PathFinder>();
+        void Update () {
+            if (pathFinder == null) {
+                pathFinder = GameObject.FindObjectOfType<PathFinder> ();
             }
 
-            string latestName = pathFinder.GetLatestWayPoint().gameObject.name;
-            int latestNum = int.Parse(latestName.Replace("point - ", ""));
+            if (pathFinder.ShowDebugRaycast) {
+                foreach (WayPoint w in Neighbors) {
+                    Vector3 line = (w.transform.position + pathFinder.GreenLineOffset) - (this.transform.position + pathFinder.GreenLineOffset);
 
-            GameObject w = GameObject.Instantiate(pathFinder.WayPointPrefab) as GameObject;
+                    if (w.transform.position.x > this.transform.position.x) {
+                        Debug.DrawRay (this.transform.position + pathFinder.GreenLineOffset, line, Color.green);
+                    } else if (w.transform.position.x < this.transform.position.x) {
+                        Debug.DrawRay (this.transform.position + pathFinder.GreenLineOffset * 0.1f, line, Color.blue);
+                    }
+                }
+            }
+        }
+
+        void CreateWayPointPrefab (Vector3 offset) {
+            if (pathFinder == null) {
+                pathFinder = this.gameObject.GetComponentInParent<PathFinder> ();
+            }
+
+            string latestName = pathFinder.GetLatestWayPoint ().gameObject.name;
+            int latestNum = int.Parse (latestName.Replace ("point - ", ""));
+
+            GameObject w = GameObject.Instantiate (pathFinder.WayPointPrefab) as GameObject;
             w.transform.parent = this.transform.parent.transform;
             w.name = "point";
             w.transform.position = this.transform.position + offset;
 
-            WayPoint wp = w.GetComponent<WayPoint>();
+            WayPoint wp = w.GetComponent<WayPoint> ();
             wp.pathFindMethod = PathFindMethod.WALK;
-            wp.Neighbors = new List<WayPoint>();
-            wp.Neighbors.Add(this);
+            wp.Neighbors = new List<WayPoint> ();
+            wp.Neighbors.Add (this);
             wp.GroundName = GroundName;
 
             //Debug.Log("latest name: " + latestName);
             //Debug.Log("latest num: " + latestNum.ToString());
 
-            wp.gameObject.name = "point - " + (latestNum + 1).ToString();
+            wp.gameObject.name = "point - " + (latestNum + 1).ToString ();
 
-            Neighbors.Add(wp);
+            Neighbors.Add (wp);
         }
 
-        public void CalcNeighborDistance()
-        {
-            foreach (WayPoint n in Neighbors)
-            {
+        public void CalcNeighborDistance () {
+            foreach (WayPoint n in Neighbors) {
                 //Debug.Log ("calculating " + this.gameObject.name + " to " + n.gameObject.name);
-                if (KnownDistance + 1 < n.KnownDistance)
-                {
+                if (KnownDistance + 1 < n.KnownDistance) {
                     n.PreviousPoint = this;
                     n.KnownDistance = KnownDistance + 1;
                     //Debug.Log ("new path found! to " + n.gameObject.name);
@@ -83,37 +91,27 @@ namespace roundbeargames
             }
         }
 
-        void OnTriggerEnter(Collider col)
-        {
-            TouchDetector det = col.GetComponent<TouchDetector>();
-            if (det != null)
-            {
-                if (det.touchDetectorType == TouchDetectorType.WAY_POINT_DETECTOR)
-                {
-                    if (det.controlMechanism.moveData != null)
-                    {
+        void OnTriggerEnter (Collider col) {
+            TouchDetector det = col.GetComponent<TouchDetector> ();
+            if (det != null) {
+                if (det.touchDetectorType == TouchDetectorType.WAY_POINT_DETECTOR) {
+                    if (det.controlMechanism.moveData != null) {
                         det.controlMechanism.moveData.LastWayPoint = this;
-                        if (det.controlMechanism.controlType == ControlType.PLAYER)
-                        {
-                            TurnOnPlayerColor();
-                        }
-                        else if (det.controlMechanism.controlType == ControlType.ENEMY)
-                        {
-                            TurnOnEnemyColor();
+                        if (det.controlMechanism.controlType == ControlType.PLAYER) {
+                            TurnOnPlayerColor ();
+                        } else if (det.controlMechanism.controlType == ControlType.ENEMY) {
+                            TurnOnEnemyColor ();
                         }
                     }
                 }
             }
         }
 
-        void OnTriggerExit(Collider col)
-        {
-            TouchDetector det = col.GetComponent<TouchDetector>();
-            if (det != null)
-            {
-                if (det.touchDetectorType == TouchDetectorType.WAY_POINT_DETECTOR)
-                {
-                    TurnOnDefaultColor();
+        void OnTriggerExit (Collider col) {
+            TouchDetector det = col.GetComponent<TouchDetector> ();
+            if (det != null) {
+                if (det.touchDetectorType == TouchDetectorType.WAY_POINT_DETECTOR) {
+                    TurnOnDefaultColor ();
                     //if (det.controlMechanism.controlType == ControlType.PLAYER) {
                     //Debug.Log ("exited: " + this.gameObject.name);
                     //}
@@ -121,19 +119,16 @@ namespace roundbeargames
             }
         }
 
-        public void TurnOnPlayerColor()
-        {
-            renderer.material.SetColor("_Color", PlayerColor);
+        public void TurnOnPlayerColor () {
+            renderer.material.SetColor ("_Color", PlayerColor);
         }
 
-        public void TurnOnEnemyColor()
-        {
-            renderer.material.SetColor("_Color", EnemyColor);
+        public void TurnOnEnemyColor () {
+            renderer.material.SetColor ("_Color", EnemyColor);
         }
 
-        public void TurnOnDefaultColor()
-        {
-            renderer.material.SetColor("_Color", DefaultColor);
+        public void TurnOnDefaultColor () {
+            renderer.material.SetColor ("_Color", DefaultColor);
         }
     }
 }
