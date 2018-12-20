5b9b533be33651a63e6afe85579a9db156126901
RoundBearChoi <roundbeargames@gmail.com>
2018-12-20 16:20:20 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/PlayerWalk.cs
+++ Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/PlayerWalk.cs
@@ -2,93 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace roundbeargames
-{
-    public class PlayerWalk : CharacterState
-    {
-        public override void InitState()
-        {
-            ANIMATION_DATA.DesignatedAnimation = PlayerState.HumanoidWalk.ToString();
+namespace roundbeargames {
+    public class PlayerWalk : CharacterState {
+        public override void InitState () {
+            ANIMATION_DATA.DesignatedAnimation = PlayerState.HumanoidWalk.ToString ();
 
-            if (!move.CanInstantTurn(characterStateController.PrevState.GetType()))
-            {
+            if (!move.CanInstantTurn (characterStateController.PrevState.GetType ())) {
 
-            }
-            else
-            {
-                MOVEMENT_DATA.Turn = move.GetTurn();
-                if (MOVEMENT_DATA.Turn != CHARACTER_TRANSFORM.rotation.eulerAngles.y)
-                {
-                    CHARACTER_TRANSFORM.rotation = Quaternion.Euler(0, MOVEMENT_DATA.Turn, 0);
+            } else {
+                MOVEMENT_DATA.Turn = move.GetTurn ();
+                if (MOVEMENT_DATA.Turn != CHARACTER_TRANSFORM.rotation.eulerAngles.y) {
+                    CHARACTER_TRANSFORM.rotation = Quaternion.Euler (0, MOVEMENT_DATA.Turn, 0);
                 }
             }
         }
 
-        public override void RunFixedUpdate()
-        {
-            MOVEMENT_DATA.Turn = move.GetTurn();
+        public override void RunFixedUpdate () {
+            MOVEMENT_DATA.Turn = move.GetTurn ();
 
-            if (ANIMATION_DATA.AnimationNameMatches)
-            {
-                if (CONTROL_MECHANISM.IsFalling())
-                {
-                    characterStateController.ChangeState((int)PlayerState.FallALoop);
+            if (ANIMATION_DATA.AnimationNameMatches) {
+                if (CONTROL_MECHANISM.IsFalling ()) {
+                    characterStateController.ChangeState ((int) PlayerState.FallALoop);
                     return;
                 }
 
-                UpdateWalk();
-            }
-            else
-            {
-                if (characterStateController.PrevState.GetType() != typeof(PlayerCrouchSneakLeft))
-                {
-                    move.MoveForward(MOVEMENT_DATA.CrouchSpeed, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
+                if (UpdateWalk ()) {
+                    return;
                 }
-                else
-                {
-                    move.MoveForward(MOVEMENT_DATA.WalkSpeed * 0.8f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
+            } else {
+                if (characterStateController.PrevState.GetType () != typeof (PlayerCrouchSneakLeft)) {
+                    move.MoveForward (MOVEMENT_DATA.CrouchSpeed, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
+                } else {
+                    move.MoveForward (MOVEMENT_DATA.WalkSpeed * 0.8f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
                 }
             }
         }
 
-        public override void RunFrameUpdate()
-        {
-            if (UpdateAnimation())
-            {
+        public override void RunFrameUpdate () {
+            if (UpdateAnimation ()) {
 
             }
         }
 
-        public override void ClearState()
-        {
+        public override void ClearState () {
 
         }
 
-        void UpdateWalk()
-        {
-            switch (move.GetMoveTransition())
-            {
+        bool UpdateWalk () {
+            switch (move.GetMoveTransition ()) {
                 case MoveTransitionStates.RUN:
-                    characterStateController.ChangeState((int)PlayerState.HumanoidRun);
-                    break;
+                    characterStateController.ChangeState ((int) PlayerState.HumanoidRun);
+                    return true;
                 case MoveTransitionStates.WALK:
-                    if (MOVEMENT_DATA.MoveDown)
-                    {
-                        characterStateController.ChangeState((int)PlayerState.CrouchedSneakingLeft);
+                    if (MOVEMENT_DATA.MoveDown) {
+                        characterStateController.ChangeState ((int) PlayerState.CrouchedSneakingLeft);
                     }
-                    float turn = move.GetTurn();
-                    move.MoveForward(MOVEMENT_DATA.WalkSpeed, turn);
-                    break;
+                    float turn = move.GetTurn ();
+                    move.MoveForward (MOVEMENT_DATA.WalkSpeed, turn);
+                    return true;
                 case MoveTransitionStates.JUMP:
-                    if (Mathf.Abs(CONTROL_MECHANISM.RIGIDBODY.velocity.y) < 0.025f)
-                    {
-                        characterStateController.ChangeState((int)PlayerState.JumpingUp);
+                    if (Mathf.Abs (CONTROL_MECHANISM.RIGIDBODY.velocity.y) < 0.025f) {
+                        characterStateController.ChangeState ((int) PlayerState.JumpingUp);
                     }
-                    break;
+                    return true;
                 case MoveTransitionStates.NONE:
-                    characterStateController.ChangeState((int)PlayerState.HumanoidIdle);
-                    break;
+                    if (MOVEMENT_DATA.MoveDown) {
+                        characterStateController.ChangeState ((int) PlayerState.CrouchIdle);
+                    } else {
+                        characterStateController.ChangeState ((int) PlayerState.HumanoidIdle);
+                    }
+
+                    return true;
             }
+
+            return false;
         }
     }
 }
