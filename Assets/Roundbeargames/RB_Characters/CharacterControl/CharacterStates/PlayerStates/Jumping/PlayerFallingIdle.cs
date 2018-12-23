759b9baa448b882f2dcadab9a5416f647f734758
RoundBearChoi <roundbeargames@gmail.com>
2018-12-23 18:18:01 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Jumping/PlayerFallingIdle.cs
+++ Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Jumping/PlayerFallingIdle.cs
@@ -2,80 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace roundbeargames
-{
-    public class PlayerFallingIdle : CharacterState
-    {
-        public override void InitState()
-        {
-            ANIMATION_DATA.DesignatedAnimation = PlayerState.FallALoop.ToString();
+namespace roundbeargames {
+    public class PlayerFallingIdle : CharacterState {
+        public override void InitState () {
+            ANIMATION_DATA.DesignatedAnimation = PlayerState.FallALoop.ToString ();
         }
 
-        public override void RunFixedUpdate()
-        {
-            MOVEMENT_DATA.Turn = move.GetTurn();
-            move.AirMove();
+        public override void RunFixedUpdate () {
+            MOVEMENT_DATA.Turn = move.GetTurn ();
+            move.AirMove ();
 
-            if (MOVEMENT_DATA.IsGrounded)
-            {
-                if (RollAfterJump())
-                {
-                    characterStateController.ChangeState((int)PlayerState.StandToRoll);
+            if (MOVEMENT_DATA.IsGrounded) {
+                if (RollAfterJump ()) {
+                    characterStateController.ChangeState ((int) PlayerState.StandToRoll);
                     return;
-                }
-                else
-                {
-                    CONTROL_MECHANISM.ClearVelocity();
-                    characterStateController.ChangeState((int)PlayerState.FallingToLanding);
+                } else {
+                    CONTROL_MECHANISM.ClearVelocity ();
+                    characterStateController.ChangeState ((int) PlayerState.FallingToLanding);
                     return;
                 }
 
             }
 
-            if (ANIMATION_DATA.AnimationNameMatches)
-            {
-                if (jump.GrabLedge())
-                {
-                    characterStateController.ChangeState((int)PlayerState.HangingIdle);
+            if (ANIMATION_DATA.AnimationNameMatches) {
+                if (jump.GrabLedge ()) {
+                    characterStateController.ChangeState ((int) PlayerState.HangingIdle);
                     return;
                 }
             }
         }
 
-        public override void RunFrameUpdate()
-        {
-            if (UpdateAnimation())
-            {
+        public override void RunFrameUpdate () {
+            if (UpdateAnimation ()) {
 
             }
         }
 
-        private bool RollAfterJump()
-        {
-            if (MOVEMENT_DATA.AirMomentum < 3.6f)
-            {
+        private bool RollAfterJump () {
+            if (MOVEMENT_DATA.AirMomentum < 4.8f) {
                 return false;
             }
 
-            if (CONTROL_MECHANISM.IsFacingForward())
-            {
-                if (MOVEMENT_DATA.MoveForward)
-                {
+            if (CONTROL_MECHANISM.IsFacingForward ()) {
+                if (MOVEMENT_DATA.MoveForward) {
                     return true;
                 }
-            }
-            else if (!CONTROL_MECHANISM.IsFacingForward())
-            {
-                if (MOVEMENT_DATA.MoveBack)
-                {
+            } else if (!CONTROL_MECHANISM.IsFacingForward ()) {
+                if (MOVEMENT_DATA.MoveBack) {
                     return true;
                 }
             }
             return false;
         }
 
-        public override void ClearState()
-        {
+        public override void ClearState () {
 
         }
     }
