d9c4664197909e728023a50309eafdd880fc2dfd
RoundBearChoi <roundbeargames@gmail.com>
2018-12-25 00:44:38 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Environment/GameCam.cs
+++ Assets/Roundbeargames/RB_Environment/GameCam.cs
@@ -1,19 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine.PostFX;
 using Sirenix.OdinInspector;
+using UnityEngine;
 
-namespace roundbeargames
-{
-    public enum CameraOffsetType
-    {
+namespace roundbeargames {
+    public enum CameraOffsetType {
         DEFAULT,
         ZOOM_ON_PLAYER_DEATH_RIGHT_SIDE,
         ZOOM_ON_PLAYER_DEATH_LEFT_SIDE,
     }
-    public class GameCam : SerializedMonoBehaviour
-    {
+    public class GameCam : SerializedMonoBehaviour {
         public UnityEngine.PostProcessing.PostProcessingBehaviour PostProc;
         public Cinemachine.CinemachineVirtualCamera cinemachineVirtualCam;
         public Cinemachine.CinemachineTransposer cinemachineTransposer;
@@ -22,50 +19,40 @@ namespace roundbeargames
         //public PlayerFollow playerFollow;
         public CinemachinePostFX postFX;
 
-        void Start()
-        {
-            PostProc = this.gameObject.GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>();
-            Cinemachine.CinemachineComponentBase[] arrPipeline = cinemachineVirtualCam.GetComponentPipeline();
-            foreach (Cinemachine.CinemachineComponentBase c in arrPipeline)
-            {
-                if (c.GetType() == typeof(Cinemachine.CinemachineTransposer))
-                {
+        void Start () {
+            PostProc = this.gameObject.GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour> ();
+            Cinemachine.CinemachineComponentBase[] arrPipeline = cinemachineVirtualCam.GetComponentPipeline ();
+            foreach (Cinemachine.CinemachineComponentBase c in arrPipeline) {
+                if (c.GetType () == typeof (Cinemachine.CinemachineTransposer)) {
                     cinemachineTransposer = c as Cinemachine.CinemachineTransposer;
                     break;
                 }
             }
 
-            SetOffset(CameraOffsetType.DEFAULT);
+            SetOffset (CameraOffsetType.DEFAULT);
         }
 
-        public void SetOffset(CameraOffsetType offsetType)
-        {
+        public void SetOffset (CameraOffsetType offsetType) {
             //cinemachineTransposer.m_FollowOffset = CamOffsets[offsetType];
-            OffsetChangeRoutine = StartCoroutine(_ProcChangeOffset(CamOffsets[offsetType]));
+            OffsetChangeRoutine = StartCoroutine (_ProcChangeOffset (CamOffsets[offsetType]));
 
-            if (offsetType == CameraOffsetType.ZOOM_ON_PLAYER_DEATH_LEFT_SIDE)
-            {
+            if (offsetType == CameraOffsetType.ZOOM_ON_PLAYER_DEATH_LEFT_SIDE) {
                 postFX.m_FocusOffset = 0.3f;
 
-            }
-            else if (offsetType == CameraOffsetType.ZOOM_ON_PLAYER_DEATH_RIGHT_SIDE)
-            {
+            } else if (offsetType == CameraOffsetType.ZOOM_ON_PLAYER_DEATH_RIGHT_SIDE) {
                 postFX.m_FocusOffset = 0.3f;
             }
         }
 
-        IEnumerator _ProcChangeOffset(Vector3 targetOffset)
-        {
-            while (true)
-            {
-                if (Mathf.Abs(Vector3.Magnitude(cinemachineTransposer.m_FollowOffset - targetOffset)) < 0.01f)
-                {
+        IEnumerator _ProcChangeOffset (Vector3 targetOffset) {
+            while (true) {
+                if (Mathf.Abs (Vector3.Magnitude (cinemachineTransposer.m_FollowOffset - targetOffset)) < 0.01f) {
                     //Debug.Log("offset change complete");
                     cinemachineTransposer.m_FollowOffset = targetOffset;
                     yield break;
                 }
-                cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetOffset, Time.deltaTime * 1.15f);
-                yield return new WaitForEndOfFrame();
+                cinemachineTransposer.m_FollowOffset = Vector3.Lerp (cinemachineTransposer.m_FollowOffset, targetOffset, Time.deltaTime * 55f);
+                yield return new WaitForEndOfFrame ();
             }
 
         }
