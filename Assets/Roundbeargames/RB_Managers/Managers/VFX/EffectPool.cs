eee239a6cc010b5b6de2ad916de19760a4877083
c <roundbeargames@gmail.com>
2018-12-24 17:19:12 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Managers/Managers/VFX/EffectPool.cs
+++ Assets/Roundbeargames/RB_Managers/Managers/VFX/EffectPool.cs
@@ -2,45 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum SimpleEffectType {
-	SPARK,
-	FLARE,
-	BLOOD,
-	GROUND_SHOCK,
-	DISTORTION,
-	GROUND_SMOKE,
+public enum SimpleEffectType
+{
+    SPARK,
+    FLARE,
+    BLOOD,
+    GROUND_SHOCK,
+    DISTORTION,
+    GROUND_SMOKE,
 }
 
-namespace roundbeargames {
-	public abstract class EffectPool : MonoBehaviour {
-		[SerializeField] SimpleEffectType simpleEffectType;
-		[SerializeField] GameObject EffectPrefab;
-		[SerializeField] List<GameObject> Showing;
-		[SerializeField] List<GameObject> Pool;
-		[SerializeField] float Duration;
+namespace roundbeargames
+{
+    public abstract class EffectPool : MonoBehaviour
+    {
+        [SerializeField] SimpleEffectType simpleEffectType;
+        [SerializeField] GameObject EffectPrefab;
+        [SerializeField] List<GameObject> Showing;
+        [SerializeField] List<GameObject> Pool;
+        [SerializeField] float Duration;
 
-		public void ShowEffect (Vector3 pos) {
-			GameObject effect;
-			if (Pool.Count == 0) {
-				effect = Instantiate (EffectPrefab) as GameObject;
-			} else {
-				effect = Pool[0];
-				Pool.RemoveAt (0);
-			}
-			//effect.transform.localPosition = Vector3.zero;
-			effect.transform.position = pos;
-			effect.SetActive (true);
-			Showing.Add (effect);
+        public void ShowEffect(Vector3 pos)
+        {
+            GameObject effect;
+            if (Pool.Count == 0)
+            {
+                effect = Instantiate(EffectPrefab) as GameObject;
+            }
+            else
+            {
+                effect = Pool[0];
+                Pool.RemoveAt(0);
+            }
+            //effect.transform.localPosition = Vector3.zero;
+            effect.transform.position = new Vector3(pos.x, pos.y, pos.z);
+            effect.SetActive(true);
+            Showing.Add(effect);
 
-			StartCoroutine (_TurnOff (Duration, effect));
-		}
+            StartCoroutine(_TurnOff(Duration, effect));
+        }
 
-		IEnumerator _TurnOff (float seconds, GameObject obj) {
-			yield return new WaitForSeconds (seconds);
-			Showing.Remove (obj);
-			Pool.Add (obj);
-			obj.SetActive (false);
-		}
+        IEnumerator _TurnOff(float seconds, GameObject obj)
+        {
+            yield return new WaitForSeconds(seconds);
+            Showing.Remove(obj);
+            Pool.Add(obj);
+            obj.SetActive(false);
+        }
 
-	}
+    }
 }
