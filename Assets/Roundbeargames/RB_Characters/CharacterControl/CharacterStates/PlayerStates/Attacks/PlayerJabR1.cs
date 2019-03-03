1245c684f5c33e639ba87d3c6d7b3153c21e2d2b
RoundBearChoi <roundbeargames@gmail.com>
2019-03-03 18:46:50 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Attacks/PlayerJabR1.cs
+++ Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Attacks/PlayerJabR1.cs
@@ -2,42 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace roundbeargames
-{
-    public class PlayerJabR1 : CharacterState
-    {
-        public override void InitState()
-        {
-            ANIMATION_DATA.DesignatedAnimation = PlayerState.Jab_R_1.ToString();
+namespace roundbeargames {
+    public class PlayerJabR1 : CharacterState {
+        public override void InitState () {
+            ANIMATION_DATA.DesignatedAnimation = PlayerState.Jab_R_1.ToString ();
         }
 
-        public override void RunFixedUpdate()
-        {
+        public override void RunFixedUpdate () {
 
         }
 
-        public override void RunFrameUpdate()
-        {
-            if (UpdateAnimation())
-            {
-                if (DurationTimePassed())
-                {
-                    characterStateController.ChangeState((int)PlayerState.HumanoidIdle);
-                    attack.DeRegister(characterStateController.controlMechanism.gameObject.name, PlayerState.Jab_R_1.ToString());
-                    //return;
+        public override void RunFrameUpdate () {
+            if (UpdateAnimation ()) {
+                if (DurationTimePassed ()) {
+                    characterStateController.ChangeState ((int) PlayerState.HumanoidIdle);
+                    attack.DeRegister (characterStateController.controlMechanism.gameObject.name, PlayerState.Jab_R_1.ToString ());
+                    return;
                 }
 
-                attack.UpdateHit(TouchDetectorType.ATTACK_RIGHT_FIST, ref attack.Target);
+                attack.UpdateHit (TouchDetectorType.ATTACK_RIGHT_FIST, ref attack.Target);
             }
         }
 
-        public override void RunLateUpdate()
-        {
+        public override void RunLateUpdate () {
 
         }
 
-        public override void ClearState()
-        {
+        public override void ClearState () {
 
         }
     }
---
+++ Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Combo1/PlayerCombo1_1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames {
+	public class PlayerCombo1_1 : CharacterState {
+		public override void InitState () {
+			ANIMATION_DATA.DesignatedAnimation = PlayerState.PlayerCombo1_1.ToString ();
+		}
+
+		public override void RunFixedUpdate () {
+
+		}
+
+		public override void RunFrameUpdate () {
+			if (UpdateAnimation ()) {
+				//Debug.Log (ANIMATION_DATA.PlayTime);
+				if (DurationTimePassed ()) {
+					characterStateController.ChangeState ((int) PlayerState.HumanoidIdle);
+					attack.DeRegister (characterStateController.controlMechanism.gameObject.name, PlayerState.Jab_R_1.ToString ());
+					return;
+				}
+
+				attack.UpdateHit (TouchDetectorType.ATTACK_RIGHT_FIST, ref attack.Target);
+			}
+		}
+
+		public override void RunLateUpdate () {
+
+		}
+
+		public override void ClearState () {
+
+		}
+	}
+}
