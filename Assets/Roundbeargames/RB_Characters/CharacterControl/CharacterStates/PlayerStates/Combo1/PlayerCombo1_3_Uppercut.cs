70abe4464285c8a8ac3ecafa7348bfefdfd0baf9
RoundBearChoi <roundbeargames@gmail.com>
2019-03-05 17:49:14 +0900

    choi
    choi

--- Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Combo1/PlayerCombo1_3_Uppercut.cs
+++ Assets/Roundbeargames/RB_Characters/CharacterControl/CharacterStates/PlayerStates/Combo1/PlayerCombo1_3_Uppercut.cs
@@ -2,74 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace roundbeargames
-{
-    public class PlayerCombo1_3_Uppercut : CharacterState
-    {
-        public override void InitState()
-        {
-            ANIMATION_DATA.DesignatedAnimation = PlayerState.PlayerCombo1_3_Uppercut.ToString();
+namespace roundbeargames {
+    public class PlayerCombo1_3_Uppercut : CharacterState {
+        public override void InitState () {
+            ANIMATION_DATA.DesignatedAnimation = PlayerState.PlayerCombo1_3_Uppercut.ToString ();
 
             //float turn = move.GetTurn ();
             //move.InstMoveForward (0.25f, turn);
 
-            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive(false);
-            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive(true);
+            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive (false);
+            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive (true);
         }
 
-        public override void RunFixedUpdate()
-        {
-            if (ANIMATION_DATA.AnimationNameMatches)
-            {
+        public override void RunFixedUpdate () {
+            if (ANIMATION_DATA.AnimationNameMatches) {
 
-            }
-            else
-            {
-                if (characterStateController.PrevState.GetType() == typeof(PlayerCombo1_2))
-                {
-                    move.MoveForward(MOVEMENT_DATA.RunSpeed * 1.2f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
+            } else {
+                if (characterStateController.PrevState.GetType () == typeof (PlayerCombo1_2)) {
+                    move.MoveForward (MOVEMENT_DATA.RunSpeed * 1.2f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
                 }
             }
         }
 
-        public override void RunFrameUpdate()
-        {
-            if (UpdateAnimation())
-            {
+        public override void RunFrameUpdate () {
+            if (UpdateAnimation ()) {
                 //Debug.Log (ANIMATION_DATA.PlayTime);
-                if (ANIMATION_DATA.PlayTime < 0.25f)
-                {
-                    move.MoveForward(MOVEMENT_DATA.RunSpeed * 0.6f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
+                if (ANIMATION_DATA.PlayTime < 0.25f) {
+                    move.MoveForward (MOVEMENT_DATA.RunSpeed * 0.6f, CHARACTER_TRANSFORM.rotation.eulerAngles.y);
 
                 }
 
-                if (DurationTimePassed())
-                {
-                    if (attack.Target == null)
-                    {
-                        characterStateController.ChangeState((int)PlayerState.HumanoidIdle);
-                        return;
-                    }
-                    else
-                    {
-                        characterStateController.ChangeState((int)PlayerState.FightIdle);
+                if (ANIMATION_DATA.PlayTime > 0.4f) {
+                    if (attack.Target != null) {
+                        characterStateController.ChangeState ((int) PlayerState.FightIdle);
                         return;
                     }
                 }
 
-                attack.UpdateHit(TouchDetectorType.ATTACK_RIGHT_FIST, ref attack.Target);
+                if (DurationTimePassed ()) {
+                    characterStateController.ChangeState ((int) PlayerState.HumanoidIdle);
+                    return;
+                }
+
+                attack.UpdateHit (TouchDetectorType.ATTACK_RIGHT_FIST, ref attack.Target);
             }
         }
 
-        public override void RunLateUpdate()
-        {
+        public override void RunLateUpdate () {
 
         }
 
-        public override void ClearState()
-        {
-            attack.DeRegister(characterStateController.controlMechanism.gameObject.name, PlayerState.PlayerCombo1_3_Uppercut.ToString());
-            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive(false);
+        public override void ClearState () {
+            attack.DeRegister (characterStateController.controlMechanism.gameObject.name, PlayerState.PlayerCombo1_3_Uppercut.ToString ());
+            CONTROL_MECHANISM.BodyTrailDictionary[BodyTrail.BACK].gameObject.SetActive (false);
         }
     }
 }
